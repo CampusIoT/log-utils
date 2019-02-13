ccd880fb081e4064e1df34dc74a298bc32574bec
Didier DONSEZ <didier.donsez@gmail.com>
2019-02-13 11:44:08 +0100

    refactor scripts

--- filter.sh
+++ filter.sh
@@ -38,7 +38,7 @@ extractGateway
 DEVEUI=1557344e72397020
 DECODER=onyield/onyield_oy1100_codec.js
 filterOnName $DEVEUI
-gzip $DEVEUI.log.gz
+gunzip $DEVEUI.log.gz
 node decodePayload.js $DEVEUI.log ../payload-codec/src/main/javascript/$DECODER > $DEVEUI.dec
 
 # ERS CO2 DEVEUI=a81758fffe03926d (IAE)
@@ -46,54 +46,65 @@ node decodePayload.js $DEVEUI.log ../payload-codec/src/main/javascript/$DECODER 
 DEVEUI=a81758fffe03926d
 DECODER=elsys/elsys_codec.js
 filterOnName $DEVEUI
-gzip $DEVEUI.log.gz
+gunzip $DEVEUI.log.gz
 node decodePayload.js $DEVEUI.log ../payload-codec/src/main/javascript/$DECODER > $DEVEUI.dec
+
+
+# ELSYS
+APPNAME=ELSYS
+DECODER=elsys/elsys_codec.js
+filterOnName $APPNAME
+gunzip $APPNAME.log.gz
+node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
 
 # LAIRD
 APPNAME=LAIRD
-DECODER=elsys/elsys_codec.js
+DECODER=laird/laird_sentriusrs_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
 
 # SENSLAB
 APPNAME=SENSLAB
 DECODER=sensinglabs/sensinglabs_senlabh_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
 
 # Adeunis FTD
 APPNAME=FTD
 DECODER=adeunisrf/adeunisrf_ftd_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
+node dec2geojson.js $APPNAME.dec > $APPNAME.geojson
 
 # Adeunis DEMOMOTE
 APPNAME=DEMOMOTE
 DECODER=adeunisrf/adeunisrf_demomote_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
+node dec2geojson.js $APPNAME.dec > $APPNAME.geojson
 
 # Semtech LORAMOTE
 APPNAME=LORAMOTE
 DECODER=semtech/semtech_loramote_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
+node dec2geojson.js $APPNAME.dec > $APPNAME.geojson
 
 # ALLORA
 APPNAME=ALLORA
 DECODER=allora/allora_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
 
 # Ascoel
 APPNAME=ASCOEL_CMTH
 DECODER=ascoel/ascoel_codec.js
 filterOnName $APPNAME
-gzip $APPNAME.log.gz
+gunzip $APPNAME.log.gz
 node decodePayload.js $APPNAME.log ../payload-codec/src/main/javascript/$DECODER > $APPNAME.dec
